0b89b336c3b0bc912fbd833c92af9a12a8060be6
Trystan Eveno <trystan.eveno@lephare.com>
2017-12-17 20:39:36 +0100

    ad clip-path polyfill

--- src/assets/app.js
+++ src/assets/app.js
@@ -1,1 +1,8 @@
+import Clipath from 'clip-path';
+
 import './main.scss';
+
+ClipPath('.ring-before', '50% 0 0 0');
+ClipPath('.ring-bigger-before', '50% 0 0 0');
+ClipPath('.ring-after', '0 0 50% 0');
+ClipPath('.ring-bigger-after', '0 0 50% 0');
